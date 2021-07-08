52701a874b9167423072cbd2fd2a21c7f6ec5cf4
Itay Podhajcer <itay.pudi@podhajcersolutions.com>
2021-07-08 14:34:55 +0300

    Changed IP allocation to static

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "azurerm_public_ip" "this" {
   name                = "pip-${local.deployment_name}"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_security_group" "this" {
